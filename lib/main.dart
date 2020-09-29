a79713eb93283198685c6a94dc2857c5223f4728
samuell324 <chistov_artem@mail.ru>
2020-09-29 14:36:54 +0300

    Create HomePage class

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primaryColor: Colors.amber
       ),
+      home: HomePage(title: title),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(),
     );
   }
 }
